c78aeeaed0bcb25b0b924205720284a5b36d5cb3
jholkeboer <jholkeboer@users.noreply.github.com>
2016-04-10 12:58:20 -0700

    add wtforms

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import render_template
+from flaskext import wtf
+from flaskext.wtf import validators
 import time
 app = Flask(__name__)
 
@@ -7,6 +10,21 @@ app = Flask(__name__)
 def hello():
     return 'Hello Cloud! The current time is: %s' % time.strftime("%I:%M:%S")
 
+@app.route('/new', methods=['GET'])
+def new():
+    return render_template('add_recipe.html')
+    
+# @app.route('/add', methods=['POST'])
+# def add():
+#     return ""
+
+# @app.route('/edit', methods['POST'])
+# def edit():
+#     return ""
+
+# @app.route('/all', methods['GET'])
+# def all():
+#     return ""
 
 @app.errorhandler(404)
 def page_not_found(e):
