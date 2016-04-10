e7d80363507a92ec3a212fa817a0fbcb072be832
jholkeboer <jholkeboer@users.noreply.github.com>
2016-04-10 15:28:37 -0700

    inserting to db

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
+import time
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
 from flaskext import wtf
 from flaskext.wtf import validators
-import time
+from google.appengine.ext import db
+from flaskext import wtf
+from flaskext.wtf import validators
+from models import Recipe
 app = Flask(__name__)
+app.secret_key='unsafe'
+app.DEBUG=True
+app.CSRF_ENABLED=True
+app.CSRF_SESSION_LKEY='unsafe'
+
+class RecipeForm(wtf.Form):
+    name = wtf.TextField('Name')
+    under30 = wtf.BooleanField('Under 30 minutes to make?')
+    category = wtf.TextField('Category')
+    ingredients = wtf.TextAreaField('Ingredients')
+    instructions = wtf.TextAreaField('Instructions')
 
 
 @app.route('/')
 def hello():
     return 'Hello Cloud! The current time is: %s' % time.strftime("%I:%M:%S")
 
-@app.route('/new', methods=['GET'])
+@app.route('/new', methods=['GET', 'POST'])
 def new():
-    return render_template('add_recipe.html')
+    form = RecipeForm()
+    if form.validate_on_submit():
+        recipe = Recipe(name = form.name.data,
+                        under30 = form.under30.data,
+                        category = form.category.data,
+                        ingredients = form.ingredients.data,
+                        instructions = form.instructions.data)
+        recipe.put()
+        print "Recipe saved."
+        return redirect('/all')
+    return render_template('add_recipe.html', form=form)
     
 # @app.route('/add', methods=['POST'])
 # def add():
@@ -22,9 +47,13 @@ def new():
 # def edit():
 #     return ""
 
-# @app.route('/all', methods['GET'])
-# def all():
-#     return ""
+@app.route('/all', methods=['GET'])
+def all():
+    # recipes = Recipe.all()
+    recipes = db.GqlQuery("SELECT * FROM Recipe")
+    for r in recipes:
+        print r
+    return render_template('view_recipes.html', recipes=recipes)
 
 @app.errorhandler(404)
 def page_not_found(e):
---
+++ models.py
@@ -0,0 +1,9 @@
+from google.appengine.ext import db
+
+
+class Recipe(db.Model):
+    name = db.StringProperty(required=True)
+    under30 = db.BooleanProperty(required=True)
+    category = db.CategoryProperty(required=True)
+    ingredients = db.TextProperty(required=True)
+    instructions = db.TextProperty(required=True)
